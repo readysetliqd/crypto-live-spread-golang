3a8734e48d483edea87d342f15f94d13c0d84f92
readyset-liqd <135415819+readysetliqd@users.noreply.github.com>
2023-12-02 13:15:19 +0000

    added ping-pong handling to keep connection alive for hyperliquidx

--- backend/exchanges/hyperliquidx/websocket.go
+++ backend/exchanges/hyperliquidx/websocket.go
@@ -11,7 +11,15 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-var pong = []byte{0} // TO DO: figure out what pong frames are
+// From the docs at https://hyperliquid.gitbook.io/hyperliquid-docs/for-developers/api/websocket#timeouts-and-heartbeats
+//
+// # Timeouts and Heartbeats
+//
+// The server will close any connection if it hasn't sent a message to it in
+// the last 60 seconds. If you are subscribing to a channel that doesn't receive
+// messages every 60 seconds, you can send heartbeat messages to keep your
+// connection alive.
+const timerDelay = 59 * time.Second
 
 // Accepts a channel and a pair. Connects to websocket api to updated channel
 // with the live bid/ask spread for that pair
@@ -34,56 +42,70 @@ func GetSpread(updateChannel chan data.Spread, pair string) {
 	log.Println(payload)
 	subscribeChannel(c, initResp, payload)
 
+	// Create a ticker that fires every const timerDelay seconds
+	timeTicker := time.NewTicker(timerDelay)
+	defer timeTicker.Stop()
+
 	resp := map[string]interface{}{}
 	var msg = []byte{}
-	// listen for the incremental updates
+	// listen for incremental updates and timeTicker countdown
 	for {
-		_, msg, err = c.ReadMessage()
-		if err != nil {
-			log.Fatal("HyperliquidX c.ReadMessage() err | ", err)
-			// TO DO: change from log.Fatal and implement reconnect
-			// c, err = attemptReconnect(initResp)
-			// subscribeChannel(c, initResp, payload)
-			// if err != nil {
-			// 	log.Fatal("HyperliquidX c.ReadMessage() err | ", err)
-			// }
-		} else if !bytes.Equal(pong, msg) { //not a pong message
-			err := json.Unmarshal(msg, &resp)
+		select {
+		case <-timeTicker.C:
+			sendPing(c)
+		default:
+			timeTicker.Reset(timerDelay)
+			_, msg, err = c.ReadMessage()
 			if err != nil {
-				log.Fatal(err)
-			} else {
-				wsReceived := time.Now()
-				var bid decimal.Decimal
-				var ask decimal.Decimal
-				var bidVolume decimal.Decimal
-				var askVolume decimal.Decimal
-				var err error
-				respInfc := resp["data"].(map[string]interface{})
-				bidsList := respInfc["levels"].([]interface{})[0]
-				asksList := respInfc["levels"].([]interface{})[1]
+				log.Fatal("HyperliquidX c.ReadMessage() err | ", err)
+				// TO DO: implement reconnect
+				// c, err = attemptReconnect(initResp)
+			} else if !bytes.Equal(nil, msg) {
+				err := json.Unmarshal(msg, &resp)
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					if resp["channel"].(string) == "pong" {
+						continue
+					}
+					wsReceived := time.Now()
+					var bid decimal.Decimal
+					var ask decimal.Decimal
+					var bidVolume decimal.Decimal
+					var askVolume decimal.Decimal
+					var err error
+					respInfc := resp["data"].(map[string]interface{})
+					bidsList := respInfc["levels"].([]interface{})[0]
+					asksList := respInfc["levels"].([]interface{})[1]
 
-				bestBid := bidsList.([]interface{})[0].(map[string]interface{})
-				bestAsk := asksList.([]interface{})[0].(map[string]interface{})
-				bidVolume, err = decimal.NewFromString(bestBid["sz"].(string))
-				if err != nil {
-					log.Fatal("decimal.NewFromString error | ", err)
+					bestBid := bidsList.([]interface{})[0].(map[string]interface{})
+					bestAsk := asksList.([]interface{})[0].(map[string]interface{})
+					bidVolume, err = decimal.NewFromString(bestBid["sz"].(string))
+					if err != nil {
+						log.Fatal("decimal.NewFromString error | ", err)
+					}
+					bid, err = decimal.NewFromString(bestBid["px"].(string))
+					if err != nil {
+						log.Fatal("decimal.NewFromString error | ", err)
+					}
+					ask, err = decimal.NewFromString(bestAsk["px"].(string))
+					if err != nil {
+						log.Fatal("decimal.NewFromString error | ", err)
+					}
+					askVolume, err = decimal.NewFromString(bestAsk["sz"].(string))
+					if err != nil {
+						log.Fatal("decimal.NewFromString error | ", err)
+					}
+					updateChannel <- data.Spread{WsReceived: wsReceived, Bid: bid, Ask: ask, BidVolume: bidVolume, AskVolume: askVolume}
 				}
-				bid, err = decimal.NewFromString(bestBid["px"].(string))
-				if err != nil {
-					log.Fatal("decimal.NewFromString error | ", err)
-				}
-				ask, err = decimal.NewFromString(bestAsk["px"].(string))
-				if err != nil {
-					log.Fatal("decimal.NewFromString error | ", err)
-				}
-				askVolume, err = decimal.NewFromString(bestAsk["sz"].(string))
-				if err != nil {
-					log.Fatal("decimal.NewFromString error | ", err)
-				}
-				updateChannel <- data.Spread{WsReceived: wsReceived, Bid: bid, Ask: ask, BidVolume: bidVolume, AskVolume: askVolume}
 			}
 		}
 	}
+}
+
+func sendPing(c *websocket.Conn) {
+	payload := `{ "method": "ping" }`
+	c.WriteMessage(1, []byte(payload))
 }
 
 func dial(initResp map[string]interface{}) (*websocket.Conn, error) {
