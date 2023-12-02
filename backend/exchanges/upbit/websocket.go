33b027df12419c09904990afb1f33bf1f7570c3e
readyset-liqd <135415819+readysetliqd@users.noreply.github.com>
2023-12-02 13:30:55 +0000

    implemented untested sendPing to keep connection alive on upbit

--- backend/exchanges/upbit/websocket.go
+++ backend/exchanges/upbit/websocket.go
@@ -11,7 +11,20 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-var pong = []byte{0} // TO DO: figure out what pong frames are
+// Roughly translated from the api docs at https://docs.upbit.com/v1.4.0/reference/connection#pingpong
+//
+// By default, the server terminates the WebSocket Connection with Idle Timeout
+// after about 120 seconds without data being displayed or sent.
+// To prevent this, you can send a PING message from the client to the server
+// to maintain the connection and view the state of the WebSocket and the
+// WebSocket connection status.
+//
+// Currently, the Bit OpenAPI WebSocket server is in progress of preparing a
+// PING Frame response, and the status of the server can be viewed through the
+// PING request/PONG response (response frame to PING) as a property of the
+// client. For configuration information, please refer to the relevant client
+// development document. (For most libraries, ping is likely built-in.)
+const timerDelay = 119 * time.Second
 
 // Accepts a channel and a pair. Connects to websocket api to updated channel
 // with the live bid/ask spread for that pair
@@ -29,40 +42,49 @@ func GetSpread(updateChannel chan data.Spread, pair string) {
 	log.Println(payload)
 	subscribeChannel(c, initResp, payload)
 
-	resp := map[string]interface{}{}
-	var msg = []byte{}
-	// listen for the incremental updates
-	for {
-		_, msg, err = c.ReadMessage()
-		if err != nil {
-			log.Fatal("Okx c.ReadMessage() err | ", err)
-			// TO DO: change from log.Fatal and implement reconnect
-			// c, err = attemptReconnect(initResp)
-			// subscribeChannel(c, initResp, payload)
-			// if err != nil {
-			// 	log.Fatal("Okx c.ReadMessage() err | ", err)
-			// }
-		} else if !bytes.Equal(pong, msg) { //not a pong message
-			err := json.Unmarshal(msg, &resp)
-			if err != nil {
-				log.Fatal(err)
-			} else {
-				log.Println(resp)
-				wsReceived := time.Now()
-				var bid decimal.Decimal
-				var ask decimal.Decimal
-				var bidVolume decimal.Decimal
-				var askVolume decimal.Decimal
-				respInfc := resp["orderbook_units"].([]interface{})[0].(map[string]interface{})
+	// Create a ticker that fires every const timerDelay seconds
+	timeTicker := time.NewTicker(timerDelay)
+	defer timeTicker.Stop()
 
-				bid = decimal.NewFromFloat(respInfc["bid_price"].(float64))
-				ask = decimal.NewFromFloat(respInfc["ask_price"].(float64))
-				bidVolume = decimal.NewFromFloat(respInfc["bid_size"].(float64))
-				askVolume = decimal.NewFromFloat(respInfc["ask_size"].(float64))
-				updateChannel <- data.Spread{WsReceived: wsReceived, Bid: bid, Ask: ask, BidVolume: bidVolume, AskVolume: askVolume}
+	resp := map[string]interface{}{}
+	// listen for incremental updates and timeTicker countdown
+	for {
+		select {
+		case <-timeTicker.C:
+			sendPing(c)
+		default:
+			timeTicker.Reset(timerDelay)
+			msgType, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Fatal("Okx c.ReadMessage() err | ", err)
+				// TO DO: change from log.Fatal and implement reconnect
+				// c, err = attemptReconnect(initResp)
+			} else if !bytes.Equal(nil, msg) && msgType != websocket.PongMessage {
+				err := json.Unmarshal(msg, &resp)
+				if err != nil {
+					log.Fatal(err)
+				} else {
+					wsReceived := time.Now()
+					var bid decimal.Decimal
+					var ask decimal.Decimal
+					var bidVolume decimal.Decimal
+					var askVolume decimal.Decimal
+					respInfc := resp["orderbook_units"].([]interface{})[0].(map[string]interface{})
+
+					bid = decimal.NewFromFloat(respInfc["bid_price"].(float64))
+					ask = decimal.NewFromFloat(respInfc["ask_price"].(float64))
+					bidVolume = decimal.NewFromFloat(respInfc["bid_size"].(float64))
+					askVolume = decimal.NewFromFloat(respInfc["ask_size"].(float64))
+					updateChannel <- data.Spread{WsReceived: wsReceived, Bid: bid, Ask: ask, BidVolume: bidVolume, AskVolume: askVolume}
+				}
 			}
 		}
 	}
+}
+
+func sendPing(c *websocket.Conn) {
+	payload := ""
+	c.WriteMessage(websocket.PingMessage, []byte(payload))
 }
 
 func dial(initResp map[string]interface{}) (*websocket.Conn, error) {
